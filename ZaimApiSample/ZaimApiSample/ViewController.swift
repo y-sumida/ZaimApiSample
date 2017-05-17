1d20014608081ef66f44a0d8d7850fa79abec009
Yuki Sumida <y.sumida.0201@gmail.com>
2017-05-17 22:11:17 +0900

    遷移時に認証済みの場合は、認証ボタンを非表示にする

--- ZaimApiSample/ZaimApiSample/ViewController.swift
+++ ZaimApiSample/ZaimApiSample/ViewController.swift
@@ -10,11 +10,39 @@ import UIKit
 import OAuthSwift
 
 class ViewController: UIViewController {
+    @IBOutlet weak var oauthView: UIView!
+
     var oauthswift: OAuthSwift?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        oauthView.isHidden = true
+
+        guard let apiKeys:  (consumerKey: String, consumerSecret: String) = readApiKeys(),
+            let token: String = UserDefaults.standard.string(forKey: "oauthToken"),
+            let secret: String = UserDefaults.standard.string(forKey: "oauthTokenSecret")
+        else {
+            oauthView.isHidden = false
+            return
+        }
+
+        let client: OAuthSwiftClient = OAuthSwiftClient(consumerKey: apiKeys.consumerKey, consumerSecret: apiKeys.consumerSecret, oauthToken: token, oauthTokenSecret: secret, version: .oauth1)
+
+        // APIコールして成功だったら認証ボタンを閉じる
+        // TODO loginの値を見る
+        _ = client.get("https://api.zaim.net/v2/home/user/verify",
+                       success: { [unowned self] response in
+                        self.oauthView.isHidden = true
+
+                        let dataString = response.string
+                        print(dataString ?? "")
+            },
+                       failure: { error in
+                        print(error)
+                        self.oauthView.isHidden = false
+        }
+        )
     }
 
     override func didReceiveMemoryWarning() {
@@ -40,23 +68,6 @@ class ViewController: UIViewController {
             return
         }
 
-        // アクセストークンからクライアント生成してAPIコール
-        let defaults = UserDefaults.standard
-        if let token: String = defaults.string(forKey: "oauthToken"), let secret: String = defaults.string(forKey: "oauthTokenSecret") {
-            let client: OAuthSwiftClient = OAuthSwiftClient(consumerKey: consumerKey, consumerSecret: consumerSecret, oauthToken: token, oauthTokenSecret: secret, version: .oauth1)
-
-            _ = client.get("https://api.zaim.net/v2/home/money",
-                           success: { response in
-                            let dataString = response.string
-                            print(dataString ?? "")
-            },
-                           failure: { error in
-                            print(error)
-            }
-            )
-            return
-        }
-
         let oauthswift = OAuth1Swift(
             consumerKey:    consumerKey,
             consumerSecret: consumerSecret,
@@ -67,7 +78,9 @@ class ViewController: UIViewController {
         self.oauthswift = oauthswift
         let _ = oauthswift.authorize(
             withCallbackURL: URL(string: "myzaimapp://oauth-callback")!,
-            success: { credential, response, parameters in
+            success: { [unowned self] credential, response, parameters in
+                self.oauthView.isHidden = true
+
                 let defaults = UserDefaults.standard
                 defaults.setValue(credential.oauthToken, forKey: "oauthToken")
                 defaults.setValue(credential.oauthTokenSecret, forKey: "oauthTokenSecret")
@@ -88,5 +101,25 @@ class ViewController: UIViewController {
         )
     }
 
+    private func readApiKeys() -> (consumerKey: String, consumerSecret: String)? {
+        // APIKey読み取り
+        guard let path: URL = Bundle.main.url(forResource: "ApiKeys", withExtension: "plist") else {
+            return nil
+        }
+
+        var consumerKey: String = ""
+        var consumerSecret: String = ""
+        do {
+            let data: Data = try Data(contentsOf: path)
+            let keys = try PropertyListSerialization.propertyList(from: data, options: [], format: nil) as? [String: Any]
+            consumerKey = keys?["consumerKey"] as! String
+            consumerSecret = keys?["consumerSecret"] as! String
+
+            return (consumerKey, consumerSecret)
+        }
+        catch {
+            return nil
+        }
+    }
 }
 
