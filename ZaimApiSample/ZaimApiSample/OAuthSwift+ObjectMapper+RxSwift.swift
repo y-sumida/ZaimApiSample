d986c29779dd333a9d4434da42ec967869f45562
Yuki Sumida <y.sumida.0201@gmail.com>
2017-05-17 23:21:44 +0900

    OAuthSwiftClientのrequestメソッドをRx化

---
+++ ZaimApiSample/ZaimApiSample/OAuthSwift+ObjectMapper+RxSwift.swift
@@ -0,0 +1,59 @@
+//
+//  OAuthSwift+ObjectMapper+RxSwift.swift
+//  ZaimApiSample
+//
+//  Created by Yuki Sumida on 2017/05/17.
+//  Copyright © 2017年 Yuki Sumida. All rights reserved.
+//
+
+import Foundation
+import OAuthSwift
+import RxSwift
+import ObjectMapper
+
+extension OAuthSwiftClient {
+
+    func rx_responseObject<T: Requestable>(request: T) -> Observable<(T.Response, HTTPURLResponse)> {
+        showRequestLog(request: request)
+
+        // TODO エラー処理
+        return Observable.create { (observer: AnyObserver<(T.Response, HTTPURLResponse)>) -> Disposable in
+            let handle: OAuthSwiftRequestHandle?  = self.request(
+                "\(request.baseURL)/\(request.path)",
+                method: request.method,
+                parameters: request.parameters,
+                success: { response in
+                    self.showResponseLog(response: response)
+                    let json = try! response.jsonObject()
+                    let model: T.Response = Mapper<T.Response>().map(JSON: json as! [String : Any])!
+                    observer.on(.next(model, response.response))
+                    observer.on(.completed)
+            },
+                failure: { error in
+                    observer.onError(error)
+            }
+            )
+
+            return Disposables.create {
+                handle?.cancel()
+            }
+        }
+    }
+    
+    private func showRequestLog<T: Requestable>(request: T) {
+        print("REQUEST--------------------")
+        print("url \(request.baseURL)/\(request.path))")
+        print("method \(request.method.rawValue)")
+        print("parameters \(request.parameters)")
+        print("---------------------------")
+    }
+    
+    private func showResponseLog(response: OAuthSwiftResponse) {
+        print("RESPONSE--------------------")
+        print("url \(String(describing: (response.request?.url?.absoluteString)))")
+        print("status \(response.response.statusCode)")
+        print("data \(String(describing: response.dataString()))")
+        print("---------------------------")
+    }
+    
+}
