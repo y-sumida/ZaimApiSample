0c81603b23a7d4ea606a3e847770a462cf10adfa
Yuki Sumida <y.sumida.0201@gmail.com>
2017-05-20 22:49:27 +0900

    支出のカテゴリコードを追加

--- ZaimApiSample/ZaimApiSample/ZaimApiCode.swift
+++ ZaimApiSample/ZaimApiSample/ZaimApiCode.swift
@@ -11,3 +11,21 @@ enum MoneyMode: String {
     case income = "income"
     case payment = "payment"
 }
+
+enum PaymentCategories : Int {
+    case food = 101
+    case dailyGoods = 102
+    case transport = 103
+    case phoneNet = 104
+    case Utilities = 105
+    case home = 106
+    case socializing = 107
+    case hobbies = 108
+    case education = 109
+    case medical = 110
+    case fashion = 111
+    case aitomobile = 112
+    case taxes = 113
+    case bigOutlay = 114
+    case other = 199
+}
