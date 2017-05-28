ead96cf2ad573013c803e109167b081ef302eb11
Yuki Sumida <y.sumida.0201@gmail.com>
2017-05-28 23:14:42 +0900

    プロフィール用テーブルビューの準備

--- ZaimApiSample/ZaimApiSample/ProfileViewController.swift
+++ ZaimApiSample/ZaimApiSample/ProfileViewController.swift
@@ -9,27 +9,40 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+
+    fileprivate let sectionTitles:[String] = ["ユーザー名", "入力回数", "入力日数"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
+extension ProfileViewController: UITableViewDelegate {
+}
+
+extension ProfileViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sectionTitles.count
+    }
+
+    func tableView(_ tableView:UITableView, titleForHeaderInSection section:Int) -> String?{
+        return sectionTitles[section]
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
