1cfaffeb1773853d0a4380c11029e2cbf915304b
Yuki Sumida <y.sumida.0201@gmail.com>
2018-01-25 23:12:05 +0900

    Codableでレスポンスを処理するためのプロトコル追加

--- ZaimApiSample/ZaimApiSample/Requestable.swift
+++ ZaimApiSample/ZaimApiSample/Requestable.swift
@@ -35,3 +35,30 @@ extension Requestable {
         return [:]
     }
 }
+
+// Temporary for ObjectMapper to Codable
+protocol Requestable2 {
+    associatedtype Response: Codable
+    var baseURL: String { get }
+    var path: String { get }
+    var method: OAuthSwiftHTTPRequest.Method { get }
+    var parameters: OAuthSwift.Parameters { get }
+}
+
+extension Requestable2 {
+    var baseURL: String {
+        return "https://api.zaim.net/v2/"
+    }
+
+    var path: String {
+        return ""
+    }
+
+    var method: OAuthSwiftHTTPRequest.Method {
+        return .GET
+    }
+
+    var parameters: OAuthSwift.Parameters {
+        return [:]
+    }
+}
