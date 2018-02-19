2f2bb57680c8f193e2acd3f24e48460d02ef8992
Yuki Sumida <y.sumida.0201@gmail.com>
2018-02-19 22:00:58 +0900

    Remove unused PaymentCategory enum.

--- ZaimApiSample/ZaimApiSample/AppConst.swift
+++ ZaimApiSample/ZaimApiSample/AppConst.swift
@@ -6,21 +6,4 @@
 //  Copyright © 2017年 Yuki Sumida. All rights reserved.
 //
 
-import Foundation
-
 let defaultApiPageLimit: Int = 20
-
-let paymentCategories: [PaymentCategory] = [
-        .food,
-        .dailyGoods,
-        .transport,
-        .phoneNet,
-        .utilities,
-        .home,
-        .socializing,
-        .hobbies,
-        .education,
-        .medical,
-        .fashion,
-        .other
-    ]
--- ZaimApiSample/ZaimApiSample/ZaimApiCode.swift
+++ ZaimApiSample/ZaimApiSample/ZaimApiCode.swift
@@ -11,35 +11,3 @@ enum MoneyMode: String, Codable {
     case income = "income"
     case payment = "payment"
 }
-
-enum PaymentCategory : Int, Codable {
-    case food = 101
-    case dailyGoods = 102
-    case transport = 103
-    case phoneNet = 104
-    case utilities = 105
-    case home = 106
-    case socializing = 107
-    case hobbies = 108
-    case education = 109
-    case medical = 110
-    case fashion = 111
-    case other = 199
-
-    var description: String {
-        switch self {
-        case .food: return "食費"
-        case .dailyGoods: return "日用雑貨"
-        case .transport: return "交通"
-        case .phoneNet: return "通信"
-        case .utilities: return "水道・光熱"
-        case .home: return "住まい"
-        case .socializing: return "交際"
-        case .hobbies: return "エンタメ"
-        case .education: return "教育"
-        case .medical: return "医療"
-        case .fashion: return "美容・衣服"
-        case .other: return "その他"
-        }
-    }
-}
